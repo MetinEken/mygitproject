1bc206422aa1c45d51db8734f89937808c451874
MetinEken <metineken2021@gmail.com>
2021-04-12 01:43:40 +0300

    12 nisan denemesi

---
+++ src/mygit_deneme/Deneme2.java
@@ -0,0 +1,10 @@
+package mygit_deneme;
+
+public class Deneme2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("12 nisan denemesi");
+	}
+
+}
