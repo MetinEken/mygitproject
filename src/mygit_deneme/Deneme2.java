21c5757f79a432c501ca60b27d3816a85af0843b
MetinEken <metineken2021@gmail.com>
2021-04-12 08:01:53 +0300

    eessg

--- src/mygit_deneme/Deneme2.java
+++ src/mygit_deneme/Deneme2.java
@@ -5,6 +5,7 @@ public class Deneme2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("12 nisan denemesi");
+		System.out.println("nasilsiniizzzz");
 	}
 
 }
